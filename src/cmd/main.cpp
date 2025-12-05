4789aaf3831f48c8e2ba755883eb1ad16f6e1dde
AliGHanizade <mralighanizade@gmail.com>
2025-12-05 08:33:30 +0330

    Refactor main menu to include all task options and improve display logic

--- src/cmd/main.cpp
+++ src/cmd/main.cpp
@@ -10,6 +10,8 @@ Version: 1.0
 #include <iostream>
 
 #include "../01-MaxNumber/MaxNumber.cpp"
+#include "../02-Divisibility/Divisibility.cpp"
+#include "../03-Fibonacci/Fibonacci.cpp"
 
 using namespace std;
 
@@ -18,17 +20,32 @@ int main(){
 
     //main var
     int NUmberOfTask;
-    string Intro = "Hi I am Write a C++ Practice";
-    string Menu = "\n Please select the task number: \n 1- Max Number Function \n   0- Exit \n Your Choice: ";
+    const int countOfTasks = 3;
+    string Menu[countOfTasks+3] ={" Please select the task number:",
+        " 1- Max Number Function",
+        " 2- Divisibility Function",
+        " 3- Fibonacci Function ",
+        " 0- Exit ",
+        "Your Choice: "} ;
 
 
 
     for (;;) {
-        cout<< Menu;
+        for (int i = 0; i < countOfTasks+3; i++)
+        {
+            cout << Menu[i] << endl;
+        }
+        
         cin >> NUmberOfTask;
          switch (NUmberOfTask) {
             case 1 :
                 MaxNumberFunction();
+            break;
+            case 2 :
+                DivisibilityFunction();
+            break;
+            case 3 :
+                FibonacciFunction();
             break;
 
             case 0 :
