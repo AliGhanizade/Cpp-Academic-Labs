8ef547dc0bebefe85ac8bcc2f3faf888af0822b0
AliGHanizade <mralighanizade@gmail.com>
2025-12-05 08:33:22 +0330

    Add FibonacciFunction to calculate and display Fibonacci series

---
+++ src/03-Fibonacci/Fibonacci.cpp
@@ -0,0 +1,23 @@
+#include "../utils/utils.h"
+#include <iostream>
+
+void FibonacciFunction() {
+    int number = GetNumber();
+    if (number <= 1){
+        std::cout << "Fibonacci number is: " << number << std::endl;
+        return;
+    }
+	
+    int prev1 = 1, prev2 = 0;
+
+  	int curr;
+    std::cout << "Fibonacci series: " << std::endl<< prev2 << " " << prev1 << " ";
+    for (int i = 1; i <= number-2; i++) {
+        std::cout << prev1 + prev2  << " ";
+        curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    std::cout << std::endl;
+    return;
+}
